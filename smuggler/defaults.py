015df26f74f3904a41f71f34d09919a55c24579a
Matt Hazinski <matt@matthazinski.com>
2017-05-10 23:59:14 -0400

    Update defaults.py

--- smuggler/defaults.py
+++ smuggler/defaults.py
@@ -2,3 +2,8 @@ DEBUG = False
 SECRET_KEY = "changeme"
 MIN_FREE_SPACE = 10737418240
 TEMP_DIR = "/tmp/smuggler"
+MOSS_URI = "http://admin:hunter2@127.0.0.1:9070"
+IMPALA_URI = "http://127.0.0.1:5000"
+# Null-padded ASCII for "digital"
+DEFAULT_UUIDS = {'format': '64696769-7461-6c00-0000-000000000000',
+                 'stack':  '00000000-0000-0000-0000-000000000000'}
