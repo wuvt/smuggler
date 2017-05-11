fd86e6a90a5956e3571eabf634e240c07baa2496
Matt Hazinski <matt@matthazinski.com>
2017-05-11 00:30:26 -0400

    Add basic API implementation

--- smuggler/api/v1/views.py
+++ smuggler/api/v1/views.py
@@ -13,8 +13,9 @@ def api_version_info():
     return jsonify({'stable': False})
 
 
-@bp.route('/holdings/<uuid:id>/music/<path:path>', methods=['POST', 'PUT'])
-def upload_track(id, path):
+@bp.route('/holding_groups/<uuid:hgid>/<uuid:hid>/music/<path:path>',
+          methods=['POST', 'PUT'])
+def upload_track(hgid, hid, path):
     # TODO open random file
     tempfile.tempdir = app.config['TEMP_DIR']
     f = tempfile.NamedTemporaryFile(delete=False, mode='wb')
@@ -26,14 +27,13 @@ def upload_track(id, path):
     # their upload succeeded in both places. If it didn't, the client must
     # rollback the changes made or overwrite them with the same UUID
     try:
-        moss_create_track(id, tmpfname, path)
-        impala_create_track(id, tmpfname, path)
+        moss_create_track(hid, tmpfname, path)
+        impala_create_track(hgid, hid, tmpfname, path)
         os.unlink(tmpfname)
-
     except:
         abort(500)
 
-    return "ok"
+    return jsonify({'message': "ok"})
 
 
 @bp.route('/holdings/<uuid:id>/lock', methods=['POST', 'PUT'])
